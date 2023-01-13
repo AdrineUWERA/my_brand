ccd0c15ed3053ff0a908f944035cc6aa3485f184
AdrineUwera <a.uwera@alustudent.com>
2023-01-13 12:24:02 +0200

    form validations

--- serverr/validations/blogValidation/updateblogSchema.js
+++ serverr/validations/blogValidation/updateblogSchema.js
@@ -1,7 +1,17 @@
 import joi from "joi";
 
 const updateblogSchema = joi.object({ 
-  content: joi.string().min(500).required().messages({
+  title: joi.string().messages({
+    "string.empty": "Blog title required.", 
+    "any.required": "Blog title required",
+  }), 
+  category: joi.string().messages({
+    "string.empty": "Category required.", 
+    "any.required": "Category required",
+  }),
+  coverImage: joi.string(),
+  references: joi.string(),
+  content: joi.string().min(500).messages({
     "string.empty": "Blog content required.", 
     "any.required": "Blog content required",
     "string.min":"The content should be at least 500 characters"
