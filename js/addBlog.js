fcad6a2060f84d4b0f1b680134f2040cd59962b6
Adrine UWERA <>
2023-01-15 20:46:42 +0200

    started working on form validation

---
+++ js/addBlog.js
@@ -0,0 +1,85 @@
+let url = "";
+var imageInput = document.getElementById("cover-image");
+imageInput.addEventListener("change", () => {
+  let fileReader = new FileReader();
+  fileReader.readAsDataURL(imageInput.files[0]);
+  fileReader.addEventListener("load", () => {
+     url = fileReader.result;
+    console.log(url);
+  });
+});
+
+var form = document
+  .getElementById("add-blog-form")
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    //gets each user input
+    var title = document.getElementById("title").value;
+    var category = document.getElementById("category").value;
+    var coverImage = document.getElementById("cover-image").value;
+    var content = document.getElementById("content").value;
+    var references = document.getElementById("references").value;
+    var submitMessage = document.getElementById("errors-success");
+    //   var regex = new RegExp(expression);
+
+    // checks if all fields are filled. If not, it i will fire an alert to tell the user to fill all fields
+    if (!title || !category || !coverImage || !content || !references) {
+      //   alert("Please fill all fields!");
+      submitMessage.innerHTML =
+        '<div id="errors" style="width: 100%; height: 40px; padding: 0px 0; margin: 0px 0; font-size: 14px; color: hsla(0, 0%, 100%, 0.7); display: flex; justify-content: center; align-items: center; background-color: hsla(10, 71%, 41%, 10%); border-radius: 3px; border: 1px solid #b1361e; >' +
+        '<p style="width: 100%; margin:0; padding: 0; text-align: center;"> Fill all fields! </p> </div>';
+    } else {
+      // otherwise, user input for each field will be stored in an object
+      //generated a unique id using date.now() because it will always be unique
+      var uniqueId = Date.now().toString();
+      //   let url="";
+
+      var newBlog = {
+        id: uniqueId,
+        title: title,
+        category: category,
+        coverImage: url,
+        category: category,
+        references: references,
+      };
+
+      // checks if there are some messages stored previously in the local storage and retrieve them if any
+      //If there no previously added messages, the newly added message will be added to the local storage
+      if (localStorage.getItem("blogs") == null) {
+        var blogs = [];
+        blogs.push(newBlog);
+        localStorage.setItem("blogs", JSON.stringify(blogs));
+        submitMessage.innerHTML =
+          '<div id="errors" style="width: 100%; height: 40px; padding: 0px 0; margin: 0px 0; font-size: 14px; color: hsla(0, 0%, 100%, 0.7); display: flex; justify-content: center; align-items: center; background-color: hsla(130, 71%, 41%, 10%); border-radius: 3px; border: 1px solid #1eb136;; >' +
+          '<p style="width: 100%; margin:0; padding: 0; text-align: center;"> Blog added! </p> </div>';
+
+        console.log(JSON.parse(localStorage.getItem("blogs")));
+        clearForm();
+      } else {
+        // localStorage.removeItem("blogs");
+        // if there are some previously stored bookmarks, then they wil be retrieved, and the newly added
+        // bookmark will be pushed to the bookmarks in the local storage
+        var blogs = JSON.parse(localStorage.getItem("blogs"));
+
+        blogs.push(newBlog);
+        localStorage.setItem("blogs", JSON.stringify(blogs));
+        submitMessage.innerHTML =
+          '<div id="errors" style="width: 100%; height: 40px; padding: 0px 0; margin: 0px 0; font-size: 14px; color: hsla(0, 0%, 100%, 0.7); display: flex; justify-content: center; align-items: center; background-color: hsla(130, 71%, 41%, 10%); border-radius: 3px; border: 1px solid #1eb136;; >' +
+          '<p style="width: 100%; margin:0; padding: 0; text-align: center;"> Blog added! </p> </div>';
+
+        console.log(JSON.parse(localStorage.getItem("blogs")));
+        clearForm();
+      }
+    }
+  });
+
+// a function to clear the form
+function clearForm() {
+  //resets the form fields
+  document.getElementById("title").value = "";
+  document.getElementById("category").value = "";
+  document.getElementById("cover-image").value = "";
+  document.getElementById("content").value = "";
+  document.getElementById("references").value = ""; 
+}
---
+++ js/login.js
@@ -0,0 +1,78 @@
+var form = document
+  .getElementById("login-form")
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    //gets each user input
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var submitMessage = document.getElementById("errors-success");
+
+    var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    //   var regex = new RegExp(expression);
+
+    // checks if all fields are filled. If not, it i will fire an alert to tell the user to fill all fields
+    if (!email || !password) {
+      //   alert("Please fill all fields!");
+      submitMessage.innerHTML =
+        '<div id="errors" style="width: 100%; height: 40px; padding: 0px 0; margin: 0px 0; font-size: 14px; color: hsla(0, 0%, 100%, 0.7); display: flex; justify-content: center; align-items: center; background-color: hsla(10, 71%, 41%, 10%); border-radius: 3px; border: 1px solid #b1361e; >' +
+        '<p style="width: 100%; margin:0; padding: 0; text-align: center;"> Fill all fields! </p> </div>';
+    }
+    // checks if the email has a correct format i.e valid
+    else if (!email.match(regex)) {
+      //   alert("Please provide a valid email!");
+      submitMessage.innerHTML =
+        '<div id="errors" style="width: 100%; height: 40px; padding: 0px 0; margin: 0px 0; font-size: 14px; color: hsla(0, 0%, 100%, 0.7); display: flex; justify-content: center; align-items: center; background-color: hsla(10, 71%, 41%, 10%); border-radius: 3px; border: 1px solid #b1361e; >' +
+        '<p style="width: 100%; margin:0; padding: 0; text-align: center;"> Invalid email! </p> </div>';
+    } else {
+      // otherwise, user input for each field will be stored in an object
+      //generated a unique id using date.now() because it will always be unique
+      // var uniqueId = Date.now().toString();
+      var loginCredentials = {
+        email: email,
+        password: password,
+      };
+
+      // checks if there are some messages stored previously in the local storage and retrieve them if any
+      //If there no previously added messages, the newly added message will be added to the local storage
+      if (
+        loginCredentials.email === "a.uwera@alustudent.com" &&
+        loginCredentials.password === "Aduwera"
+      ) {
+        window.location.href = "admin-dashboard.html";
+      } else if (localStorage.getItem("users") == null) {
+        submitMessage.innerHTML =
+          '<div id="errors" style="width: 100%; height: 40px; padding: 0px 0; margin: 0px 0; font-size: 14px; color: hsla(0, 0%, 100%, 0.7); display: flex; justify-content: center; align-items: center; background-color: hsla(130, 71%, 41%, 10%); border-radius: 3px; border: 1px solid #1eb136;; >' +
+          '<p style="width: 100%; margin:0; padding: 0; text-align: center;"> User doesnot exist! </p> </div>';
+        clearForm();
+      } else {
+        // localStorage.removeItem("messages");
+        // if there are some previously stored bookmarks, then they wil be retrieved, and the newly added
+        // bookmark will be pushed to the bookmarks in the local storage
+        var users = JSON.parse(localStorage.getItem("users"));
+        for (let i = 0; i < users.length; i++) {
+          if (
+            users[i].email === loginCredentials.email &&
+            users[i].password === loginCredentials.password
+          ) {
+            submitMessage.innerHTML =
+              '<div id="errors" style="width: 100%; height: 40px; padding: 0px 0; margin: 0px 0; font-size: 14px; color: hsla(0, 0%, 100%, 0.7); display: flex; justify-content: center; align-items: center; background-color: hsla(130, 71%, 41%, 10%); border-radius: 3px; border: 1px solid #1eb136;; >' +
+              '<p style="width: 100%; margin:0; padding: 0; text-align: center;"> User logged in </p> </div>';
+            window.location.href = "index.html";
+          }
+
+          submitMessage.innerHTML =
+            '<div id="errors" style="width: 100%; height: 40px; padding: 0px 0; margin: 0px 0; font-size: 14px; color: hsla(0, 0%, 100%, 0.7); display: flex; justify-content: center; align-items: center; background-color: hsla(130, 71%, 41%, 10%); border-radius: 3px; border: 1px solid #1eb136;; >' +
+            '<p style="width: 100%; margin:0; padding: 0; text-align: center;"> User doesnot exist! </p> </div>';
+          clearForm();
+        }
+      }
+    }
+  });
+
+// a function to clear the form
+function clearForm() {
+  //resets the form fields
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+}
